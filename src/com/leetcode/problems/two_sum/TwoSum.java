22ad85e5a5261d10154588c92166a36b3560c541
peter <bonep@hotmail.com>
2016-07-29 20:31:00 +0800

    Work with a better solution using HashMap

--- src/com/leetcode/problems/two_sum/TwoSum.java
+++ src/com/leetcode/problems/two_sum/TwoSum.java
@@ -7,13 +7,60 @@ package com.leetcode.problems.two_sum;
  *
  */
 
+import java.util.HashMap;
+import java.util.Map;
+
 class TwoSum
 {
     int[] twoSum(int[] numbers, int target)
     {
-        for (int i=0; i<numbers.length-1; i++)
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++)
         {
-            for (int j=i+1; j<numbers.length; j++)
+            int complement = target - numbers[i];
+
+            if (map.containsKey(complement))
+            {
+                return new int[]{map.get(complement), i};
+            }
+
+            map.put(numbers[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    @SuppressWarnings("unused")
+    int[] twoSum_solution_2(int[] numbers, int target)
+    {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            map.put(numbers[i], i);
+
+        }
+
+        for (int i = 0; i < numbers.length; i++)
+        {
+            int complement = target - numbers[i];
+            if (map.containsKey(complement) && map.get(complement) != i)
+            {
+                return new int[]{i, map.get(complement)};
+            }
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+    @SuppressWarnings("unused")
+    int[] twoSum_solution_1(int[] numbers, int target)
+    {
+        for (int i = 0; i < numbers.length - 1; i++)
+        {
+            for (int j = i + 1; j < numbers.length; j++)
             {
                 if (numbers[i] + numbers[j] == target)
                 {
